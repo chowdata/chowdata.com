7d6a19d8b44778c12f4443d387e36fa08da26c74
chowdata <anthony@chowdata.com>
2015-03-23 13:33:08 -0500

    added migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150323162117) do
+ActiveRecord::Schema.define(version: 20150323182922) do
 
   create_table "businesses", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -31,8 +31,9 @@ ActiveRecord::Schema.define(version: 20150323162117) do
   end
 
   create_table "profiles", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "business_id"
   end
 
   create_table "users", force: :cascade do |t|
