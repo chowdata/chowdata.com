add057f6bfdacfc53f301ecab9a8d54f5ddc286b
chowdata <anthony@chowdata.com>
2015-03-23 11:18:36 -0500

    added private user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ActiveRecord::Base
+
+  before_create :build_default_business
+  has_one :business
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+private
+  def build_default_business
+    self.build_business
+    self.save!
+  end
+
+ 
 end
